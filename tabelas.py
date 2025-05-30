bb281c65f250d8c9bb6eff9c382c5a19546e79a6
Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-05-30 16:31:42 -0300

    Update tabelas.py

--- tabelas.py
+++ tabelas.py
@@ -19,7 +19,7 @@ def render_tabelas(df_fat, df_ped, hoje):
         ult_fat['Data Emissão'] = ult_fat['Data Emissão'].dt.strftime('%d/%m/%Y')
         ult_fat_view = ult_fat[['Data Emissão', 'Cliente', 'Vendedor', 'Total Produto']].head(10)
         ult_fat_view['Total Produto'] = ult_fat_view['Total Produto'].apply(lambda x: f"R$ {x:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
-        st.dataframe(ult_fat_view, height=table_height)
+        st.dataframe(ult_fat_view.reset_index(drop=True), height=table_height)
 
     with col_ped:
         st.markdown("### Últimos Pedidos Inclusos")
@@ -27,4 +27,4 @@ def render_tabelas(df_fat, df_ped, hoje):
         ult_ped['Data Emissao'] = ult_ped['Data Emissao'].dt.strftime('%d/%m/%Y')
         ult_ped_view = ult_ped[['Data Emissao', 'Cliente', 'Vendedor', 'Valor Receita Bruta Pedido']].head(10)
         ult_ped_view['Valor Receita Bruta Pedido'] = ult_ped_view['Valor Receita Bruta Pedido'].apply(lambda x: f"R$ {x:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
-        st.dataframe(ult_ped_view, height=table_height)
+        st.dataframe(ult_ped_view.reset_index(drop=True), height=table_height)
