f33f08e5bc6d1cb23164b981587e8488e7ed0ff3
Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-05-30 16:37:49 -0300

    Update tabelas.py

--- tabelas.py
+++ tabelas.py
@@ -3,7 +3,10 @@ import streamlit as st
 def render_tabelas(df_fat, df_ped, hoje):
     table_height = 450
 
-    # Aplicar estilo global maior
+    # Campos considerados nas consultas SQL:
+    # → Faturamento: coluna do número da nota fiscal (NF) = 'DocNum', data = 'Data Emissão'
+    # → Pedidos: coluna do número do pedido (PV) = 'Pedido', data = 'Data Emissao'
+
     st.markdown("""
         <style>
         .css-1d391kg .e1f1d6gn2 {font-size: 18px;} /* Título das tabelas */
@@ -17,7 +20,8 @@ def render_tabelas(df_fat, df_ped, hoje):
         st.markdown("### Últimos Faturamentos")
         ult_fat = df_fat.sort_values(by='Data Emissão', ascending=False)
         ult_fat['Data Emissão'] = ult_fat['Data Emissão'].dt.strftime('%d/%m/%Y')
-        ult_fat_view = ult_fat[['Data Emissão', 'Cliente', 'Vendedor', 'Total Produto']].head(10)
+        ult_fat_view = ult_fat[['DocNum', 'Data Emissão', 'Cliente', 'Vendedor', 'Total Produto']].head(10)
+        ult_fat_view = ult_fat_view.rename(columns={'DocNum': 'NF'})
         ult_fat_view['Total Produto'] = ult_fat_view['Total Produto'].apply(lambda x: f"R$ {x:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
         st.dataframe(ult_fat_view, height=table_height, use_container_width=True, hide_index=True)
 
@@ -25,6 +29,7 @@ def render_tabelas(df_fat, df_ped, hoje):
         st.markdown("### Últimos Pedidos Inclusos")
         ult_ped = df_ped.sort_values(by='Data Emissao', ascending=False)
         ult_ped['Data Emissao'] = ult_ped['Data Emissao'].dt.strftime('%d/%m/%Y')
-        ult_ped_view = ult_ped[['Data Emissao', 'Cliente', 'Vendedor', 'Valor Receita Bruta Pedido']].head(10)
+        ult_ped_view = ult_ped[['Pedido', 'Data Emissao', 'Cliente', 'Vendedor', 'Valor Receita Bruta Pedido']].head(10)
+        ult_ped_view = ult_ped_view.rename(columns={'Pedido': 'PV'})
         ult_ped_view['Valor Receita Bruta Pedido'] = ult_ped_view['Valor Receita Bruta Pedido'].apply(lambda x: f"R$ {x:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
         st.dataframe(ult_ped_view, height=table_height, use_container_width=True, hide_index=True)
