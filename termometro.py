00df5f265b58712d19914df856b233e7f094faa6
Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-05-30 17:12:50 -0300

    Update termometro.py

--- termometro.py
+++ termometro.py
@@ -3,23 +3,21 @@ import plotly.graph_objects as go
 
 def render_termometro(df_fat, df_cart, META_MENSAL, hoje):
     data_col_fat = 'Data Emissão'
+    data_col_cart = 'Data Entrega'
     mes_atual = hoje.month
     ano_atual = hoje.year
 
     df_fat_mes = df_fat[(df_fat[data_col_fat].dt.month == mes_atual) & (df_fat[data_col_fat].dt.year == ano_atual)]
     realizado = df_fat_mes['Total Produto'].sum()
-    carteira = df_cart['Valor Receita Bruta Pedido'].sum()
+
+    df_cart_mes = df_cart[(df_cart[data_col_cart].dt.month == mes_atual) & (df_cart[data_col_cart].dt.year == ano_atual)]
+    carteira = df_cart_mes['Valor Receita Bruta Pedido'].sum()
+
     restante = max(META_MENSAL - realizado - carteira, 0)
 
     perc_realizado = min(realizado / META_MENSAL * 100, 100)
     perc_carteira = min(carteira / META_MENSAL * 100, 100)
     perc_restante = max(100 - perc_realizado - perc_carteira, 0)
-
-    # Exibir os valores calculados no Streamlit
-    st.write(f"✅ **Realizado:** R$ {realizado:,.2f}")
-    st.write(f"📦 **Carteira:** R$ {carteira:,.2f}")
-    st.write(f"⏳ **Restante:** R$ {restante:,.2f}")
-    st.write(f"🎯 **Meta Mensal:** R$ {META_MENSAL:,.2f}")
 
     fig_termo = go.Figure()
     fig_termo.add_trace(go.Bar(y=['Meta'], x=[realizado], orientation='h', marker=dict(color='#2ca02c'), text=[f'{perc_realizado:.1f}%'], textposition='auto', textfont=dict(size=18)))
