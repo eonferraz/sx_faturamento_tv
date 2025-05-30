4ae3dec3acdb1b68cae511b62c8e08099c225a21
Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-05-30 18:08:32 -0300

    Update titulo.py

--- titulo.py
+++ titulo.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import locale
+
+# Define locale para português
+locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
 
 def render_titulo(hoje):
-    st.markdown(f"<h2 style='text-align:center;'>Faturamento {hoje.strftime('%B').capitalize()} - SX Lighting</h2>", unsafe_allow_html=True)
+    mes_portugues = hoje.strftime('%B').capitalize()
+    st.markdown(f"<h2 style='text-align:center;'>Faturamento {mes_portugues} - SX Lighting</h2>", unsafe_allow_html=True)
