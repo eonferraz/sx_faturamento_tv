f0cf2e5bacfea1fc3ed45abd0876675ab66f0fad
Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-05-30 15:53:57 -0300

    Update titulo.py

--- titulo.py
+++ titulo.py
@@ -2,4 +2,3 @@ import streamlit as st
 
 def render_titulo(hoje):
     st.markdown(f"<h2 style='text-align:center;'>Faturamento {hoje.strftime('%B').capitalize()} - SX Lighting</h2>", unsafe_allow_html=True)
-
